d659c94d6c022941054bc01587dfa4371436756f
Nazish1712 <nazish1712@gmail.com>
2026-08-01 13:36:13 +0530

    completed speed section first skeleton

--- src/featuresSecondary/skeletons/FirstSkeleton.jsx
+++ src/featuresSecondary/skeletons/FirstSkeleton.jsx
@@ -1,28 +1,64 @@
 import { motion } from "motion/react"
-import { FileIcon, SalesforceIcon , HubspotIcon} from "@/icons"
+import { FileIcon, SalesforceIcon , HubspotIcon, SheetsIcon, CogIcon, ErrorIcon} from "@/icons"
 
 export const SkeletonOne = () => {
     return(
-     <div className="flex-1 rounded-t-3xl bg-neutral-100  border border-neutral-200 max-w-sm mx-auto w-full h-full absolute inset-x-0 p-2">
-        <Card title=""/>
+     <div className="flex-1 gap-2 flex flex-col rounded-t-3xl bg-neutral-100  border border-neutral-200 max-w-sm mx-auto w-full h-full absolute inset-x-0 p-2">
+        <Card 
+        topIcon = {<FileIcon className="size-4  text-white"/>}
+        title="Human-in-the-Loop" 
+        description ="Add reviews, approvals and escalations without slowing work."
+        tags={[
+        {text:"Salesforce" , icon:<SalesforceIcon className="size-3"/>},
+        {text:"Hubspot" , icon:< HubspotIcon className="size-3"/>},
+        {text:"Sheet" , icon:<SheetsIcon className="size-3"/>},
+        ]}/>
+
+        <Card 
+        topIcon = {<CogIcon className="size-4  text-white"/>}
+        title="Define Processing Logic" 
+        description ="Create workflows, decision points, and conditional actions for each task."
+        tags={[
+        {text:"Salesforce" , icon:<SalesforceIcon className="size-3"/>},
+        {text:"Hubspot" , icon:< HubspotIcon className="size-3"/>},
+        {text:"Sheet" , icon:<SheetsIcon className="size-3"/>},
+        ]}/> 
+
+        <Card 
+        topIcon = {<ErrorIcon className="size-4  text-white"/>}
+        title="Connect Data" 
+        description ="Link CRMs, helpdesks, and APIs to give agents secure, role-based access."
+        tags={[
+        {text:"Salesforce" , icon:<SalesforceIcon className="size-3"/>},
+        {text:"Hubspot" , icon:< HubspotIcon className="size-3"/>},
+        {text:"Sheet" , icon:<SheetsIcon className="size-3"/>},
+        ]}/>    
      </div>
     )
 }
 
 const Card = ({topIcon, title, description, tags}) => {
+    const randomColors = [
+        "bg-blue-500",
+        "bg-green-500",
+        "bg-red-500",
+    ]
+
+    const randomBg = randomColors[Math.floor(Math.random() * randomColors.length)]
+
     return(
     <div className="p-4 gap-4 shadow-black/10 border border-transparent bg-white ring-1 ring-black/10 rounded-[16px] flex items-start">
-   <div className="size-6 shrink-0 rounded-full bg-red-500 mt-1 flex items-center justify-center">
-    {/* <FileIcon className="size-4  text-white"/> */}
+   <div className={`size-6 shrink-0 bg-blue-500 rounded-full mt-1 flex items-center justify-center ${randomBg}`}
+   >
     {topIcon}
    </div>
    <div>
     <p className="text-lg font-bold text-neutral-800">{title}</p>
     <p className="text-base text-neutral-600">{description}</p>
     <div className="mt-2 flex flex-row flex-wrap gap-2">
-    <Tag text="Salesforce" icon={<SalesforceIcon className="size-3"/>}/>
-    <Tag text="HubspotIcon" icon={<HubspotIcon className="size-3"/>}/>
-    <Tag text="Sheets" icon={<HubspotIcon className="size-3"/>}/>
+    {tags.map((tag)=>(
+        <Tag key={tag.text} text={tag.text} icon={tag.icon}/>
+    ))}
     </div>
    </div>
    </div>)
